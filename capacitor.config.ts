a46ae2d76dbf9a5c5c12ccaa769a819786c5a3c7
aufasyaihan <aufa.azzahidi@mhs.unsoed.com>
2023-11-17 14:20:40 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic_crud',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
